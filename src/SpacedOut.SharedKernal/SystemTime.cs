e733f74d32969e0dda8dea46af974ab816f9ef95
AJ Hoffer <aj@hoffer.fm>
2020-12-12 16:29:19 -0500

    improved systemtime

--- src/SpacedOut.SharedKernal/SystemTime.cs
+++ src/SpacedOut.SharedKernal/SystemTime.cs
@@ -4,6 +4,36 @@ namespace SpacedOut.SharedKernel
 {
     public static class SystemTime
     {
-        public static readonly Func<DateTime> UtcNow = () => DateTime.UtcNow;
+        [ThreadStatic]
+        private static DateTime? _dateTimeUtc;
+
+        public static DateTime UtcNow()
+        {
+            if (_dateTimeUtc.HasValue)
+            {
+                return _dateTimeUtc.Value;
+            }
+
+            return DateTime.UtcNow;
+        }
+
+        public static IDisposable LockSystemTime(DateTime dateTimeUtc)
+        {
+            if (_dateTimeUtc.HasValue) throw new InvalidOperationException("SystemTime is already locked");
+
+            _dateTimeUtc = dateTimeUtc;
+
+            return new LockedDateTime();
+        }
+
+        private class LockedDateTime : IDisposable
+        {
+            public void Dispose()
+            {
+                _dateTimeUtc = null;
+
+                GC.SuppressFinalize(this);
+            }
+        }
     }
 }
